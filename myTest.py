2c33069219c4b03328f7dfcf5b121c42dd1b10c0
ali97bajelan <ali97bajelan@gmail.com>
2021-08-23 01:50:10 +0430

    kj

--- myTest.py
+++ myTest.py
@@ -3,3 +3,4 @@ print(django.get_version())
 
 # user: ali -- email: ali97@gmail.com -- pass: ali12345
 # upRBrN79uh5kxQE4cN6B
+# vscode://vscode.github-authentication/did-authenticate?windowid=1&code=c8e81c28082ce5e0e7d7&state=14ff2994-be2f-436e-a0a1-08d25182853a
