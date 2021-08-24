2abc9480259255fcfe6d9814d0560f295e3fc438
ali97bajelan <ali97bajelan@gmail.com>
2021-08-24 19:14:24 +0430

    ...

---
+++ miniblog/blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=15)
+    password = forms.CharField(widget=forms.PasswordInput())
+    '''
+    def clean_message(self):
+        username = self.cleaned_data.get("username")
+        dbuser = User.objects.filter(username = username)
+        
+        if not dbuser:
+            raise forms.ValidationError("User does not exist in our db!")
+        return username
+    '''
